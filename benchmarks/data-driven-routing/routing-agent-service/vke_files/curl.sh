781968615e8897d4741ea8b54507504a83bb545e
gangmuk <gangmuk@gmail.com>
2025-10-28 09:46:27 +0000

    sycn leftover including plot script
    Signed-off-by: gangmuk <gangmuk@gmail.com>

--- benchmarks/data-driven-routing/routing-agent-service/vke_files/curl.sh
+++ benchmarks/data-driven-routing/routing-agent-service/vke_files/curl.sh
@@ -12,12 +12,14 @@ llm_model="llama-3-8b-instruct"
 #     -H "routing-strategy: ${ROUTING_STRATEGY}" \
 #     -d '{"model": "llama3-1-8b", "subAlgorithm": "'"${subAlgorithm}"'", "messages": [{"role": "user", "content": "Where is Champaign? I am planning to visit the town. Recommend me some interesting things to do."}], "temperature": 0.0, "max_tokens": 10000, "min_tokens": 10000, "ignore_eos": true}'
 
+random_request_id=$((RANDOM % 1000))
+routing_policy="preble"
 
 curl -i -v http://115.190.180.7:80/v1/chat/completions \
     -H "Content-Type: application/json" \
-    -H "request-id: 0" \
+    -H "request-id: ${random_request_id}" \
     -H "Authorization: Bearer sk-kFJ12nKsFVfVmGpj3QzX65s4RbN2xJqWzPYCjYu7wT3BlbLi" \
-    -H "routing-strategy: rl-online-router" \
+    -H "routing-strategy: ${routing_policy}" \
     -d '{
         "model": "'"${llm_model}"'",
         "subAlgorithm": "'"${subAlgorithm}"'",
--- benchmarks/data-driven-routing/routing-agent-service/vke_files/plot-all-compare.sh
+++ benchmarks/data-driven-routing/routing-agent-service/vke_files/plot-all-compare.sh
@@ -2,11 +2,20 @@
 
 set -e
 
+# target_dir_list=(
+#     "../workload-and-experiment_results/SharingRatio71%/hand-picked"
+#     "../workload-and-experiment_results/SharingRatio47%/hand-picked"
+#     "../workload-and-experiment_results/SharingRatio28%/hand-picked"
+#     "../workload-and-experiment_results/SharingRatio9%/hand-picked"
+# )
+
+
 target_dir_list=(
-    "../workload-and-experiment_results/SharingRatio71%/hand-picked"
-    "../workload-and-experiment_results/SharingRatio47%/hand-picked"
-    "../workload-and-experiment_results/SharingRatio28%/hand-picked"
-    "../workload-and-experiment_results/SharingRatio9%/hand-picked"
+    "../workload-and-experiment_results/for_paper/SharingRatio71%"
+    "../workload-and-experiment_results/for_paper/SharingRatio47%"
+    "../workload-and-experiment_results/for_paper/SharingRatio28%"
+    "../workload-and-experiment_results/for_paper/SharingRatio9%"
+    "../workload-and-experiment_results/for_paper/MixedSharingRatio"
 )
 
 for target_dir in "${target_dir_list[@]}"; do
